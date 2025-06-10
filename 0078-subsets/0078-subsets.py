9f0ab257f16ff73c6b116f28fcad61135cb32d88
Aksaya Venugopal <86884983+AksayaVenugopal@users.noreply.github.com>
2025-06-10 22:52:51 +0530

    Time: 2 ms (12.03%), Space: 17.9 MB (65.15%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,12 +1,30 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n=len(nums)
-        o=[]
-        for i in range((2**n)):
-            temp=[]
-            for j in range(n):
-                if i&(1<<j):
-                    temp.append(nums[j])
-            o.append(temp)
-        return o
+ #       n=len(nums)
+  #      o=[]
+   #     for i in range((2**n)):
+    #        temp=[]
+     #       for j in range(n):
+      #          if i&(1<<j):
+       #             temp.append(nums[j])
+        #    o.append(temp)
+       # return o
+
+
+
+
+        a=[]
         
+        def f(ind,arr,nums,a):
+            
+            if ind>=len(nums):
+                print(arr)
+                a.append(arr[:])
+                return
+            arr.append(nums[ind])
+            f(ind+1,arr,nums,a)
+            arr.remove(nums[ind])
+            f(ind+1,arr,nums,a)
+        s=[]
+        f(0,s,nums,a)
+        return a
